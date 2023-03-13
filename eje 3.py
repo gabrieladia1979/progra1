7fe3567f3a793cfb3217ac50611b163d078c9803
gabrieladia1979 <111798995+gabrieladia1979@users.noreply.github.com>
2023-03-13 17:29:47 -0300

    Add files via upload

---
+++ eje 3.py
@@ -0,0 +1,14 @@
+def descuento(a):
+    valor = 50
+    if a > 1 and a <= 20:
+        montoTotal = valor * a
+    elif a > 20 and a <= 30:
+        
+
+
+
+
+cantidadDeViajes = int(input("Ingrese la cantidad de vaijes al mes:"))
+while cantidadDeViajes < 1:
+    cantidadDeViajes = int(input("Ingrese la cantidad de vaijes correcta:"))
+    
