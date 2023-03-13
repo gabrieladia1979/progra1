cc9f2ed20370fd3a3e6e87835d90da9d45e0d38c
gabrieladia1979 <111798995+gabrieladia1979@users.noreply.github.com>
2023-03-13 16:11:43 -0300

    Add files via upload

---
+++ eje1.py
@@ -0,0 +1,14 @@
+funcion(a):
+    if a>0:
+        numero=a
+        return(numero)
+        
+num=int(input("Ingrese un numero")
+funcion(num)
+num2=int(input("Ingrese un numero")
+funcion(num2)
+num3=int(input("Ingrese un numero")
+funcion(num3)
+
+        
+    
