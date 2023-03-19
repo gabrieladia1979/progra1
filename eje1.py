a8b8675ece9fc8401125b4f90f876fa228a5398a
gabrieladia1979 <111798995+gabrieladia1979@users.noreply.github.com>
2023-03-19 17:11:05 -0300

    Update eje1.py

--- eje1.py
+++ eje1.py
@@ -1,14 +1,25 @@
-funcion(a):
-    if a>0:
-        numero=a
-        return(numero)
-        
-num=int(input("Ingrese un numero")
-funcion(num)
-num2=int(input("Ingrese un numero")
-funcion(num2)
-num3=int(input("Ingrese un numero")
-funcion(num3)
-
-        
+def mayor (a,b,c):
+    lista=[a,b,c]
+    may=a
     
+    if a==b and b==c:
+        return ("No hay un valor mayor estricto, son todos iguales")
+    else:
+        for i in range(len(lista)):
+            if may < lista[i]:
+                may=lista[i]
+        return may
+        
+num=int(input("Ingrese un numero:"))
+while num < 0:
+         num=int(input("Ingrese un numero positivo"))
+                 
+num2=int(input("Ingrese un numero"))
+while num2 < 0:
+         num2=int(input("Ingrese un numero positivo"))
+                 
+num3=int(input("Ingrese un numero"))
+while num3 < 0:
+         num3=int(input("Ingrese un numero positivo"))
+                 
+print(mayor(num,num2,num3))
