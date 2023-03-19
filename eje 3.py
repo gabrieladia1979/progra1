eae2a95635276a84bfb95fd5261ec3ab458f81a8
gabrieladia1979 <111798995+gabrieladia1979@users.noreply.github.com>
2023-03-19 18:16:44 -0300

    Update eje 3.py

--- eje 3.py
+++ eje 3.py
@@ -1,14 +1,29 @@
 def descuento(a):
-    valor = 50
+    valor = 25
     if a > 1 and a <= 20:
         montoTotal = valor * a
     elif a > 20 and a <= 30:
-        
+        montoTotal = valor * a
+        a = a - 20
+        montoTotal = montoTotal + a * (valor-(valor*0.2))
+    elif a > 30 and a <= 40:
+        montoTotal = valor * a
+        a = a - 20
+        montoTotal = montoTotal + a * (valor-(valor*0.2))
+        a = a - 10
+        montoTotal = montoTotal + a * (valor-(valor*0.3))
+    elif a >=40:
+        montoTotal = valor * a
+        a = a - 20
+        montoTotal = montoTotal + a * (valor-(valor*0.2))
+        a = a - 10
+        montoTotal = montoTotal + a * (valor-(valor*0.3))
+        a = a - 10
+        montoTotal = montoTotal + a * (valor-(valor*0.4))
+    return montoTotal
 
-
-
-
-cantidadDeViajes = int(input("Ingrese la cantidad de vaijes al mes:"))
+cantidadDeViajes = int(input("Ingrese la cantidad de viajes al mes:"))
 while cantidadDeViajes < 1:
     cantidadDeViajes = int(input("Ingrese la cantidad de vaijes correcta:"))
-    
+
+print(descuento(cantidadDeViajes))
