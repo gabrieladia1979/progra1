a4fc62e4bf8bc322b3e5b936cf77b8e5027c9a5d
gabrieladia1979 <111798995+gabrieladia1979@users.noreply.github.com>
2023-05-08 08:55:11 -0300

    Add files via upload

--- TP04-09.py
+++ TP04-09.py
@@ -1,36 +1,25 @@
 """TP04-09 | FUNCIÓN ELIMINAR SUBCADENA
-Escribir  una  función  para eliminar  una  subcadena  de  una  cadena  de  caracteres,
-apartir  de  una  posición  y cantidad  de caracteres dadas, devolviendo la cadena resultante.
+Escribir una función para eliminar una subcadena de una cadena de caracteres, a partir de una posición y cantidad de caracteres dadas, devolviendo la cadena resultante. Escribir también un programa para verificar el comportamiento de la misma. Escribir una función para cada uno de los siguientes casos:
+A. Utilizando rebanadas
+B. Sin utilizar rebanadas"""
 
-Escribir también un programa para verificar el comportamiento de la misma.
-
-Escribir una función para cada uno de los siguientes casos:
-
-A.Utilizando rebanadas
-B.Sin utilizar rebanadas"""
-
-
-def main():
-    cad = str(input("Ingresar un cadena cualquiera de caracteres: "))
-    posicion = int(input("Ingresar la posicion por donde quiere empezar: "))
-    cantidad = int(input("Ingresar cantidad  de caracteres dadas: "))
+def eliminarRebanadas(cad,posc,cant):
     
-    return cad,posicion,cantidad
-
-def rebanadas(cad,posicion,cantidad): 
-    cadRebanada = cad[posicion:cantidad + posicion]
+    cadNueva = cad.lower()
+    cadResultantate = cadNueva[:posc] + cadNueva[posc+cant:]
     
-    return cadRebanada
+    return cadResultantate
 
-def noRebanadas(cadena,posicion,cantidad):
-    subcadena = ""
-    for i in range(len(cadena)):
-        if i >= posicion and i <(posicion+cantidad):
-            subcadena = subcadena + cadena[i]
-            
-    return subcadena
-            
-
-cadena , posc , cant = main()
-print(rebanadas(cadena,posc,cant))
-print(noRebanadas(cadena,posc,cant))
+def eliminarSinRebanadas(cad,posc,cant):
+    nuevaCadena = ""
+    for i in range(len(cad)):
+        if i < posc or i >= posc + cant:
+            nuevaCadena += cad[i]
+    return nuevaCadena
+        
+cad = "Hola que tal muy buenas gente del señor"
+print(cad)
+posc = 5
+cant = 9
+print(eliminarRebanadas(cad,posc,cant))
+print(eliminarSinRebanadas(cad,posc,cant))
