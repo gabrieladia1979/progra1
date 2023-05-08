af6ef48ab8f1edda48a63d8b25876bab0d3dc9e4
gabrieladia1979 <111798995+gabrieladia1979@users.noreply.github.com>
2023-05-08 08:55:31 -0300

    Update TP04-07.py

--- TP04-07.py
+++ TP04-07.py
@@ -2,3 +2,6 @@
 Escribir una función que reciba una cadena que contiene un número entero de muchos dígitos
 y devuelva una cadena con el mismo número, pero con los puntos de las separaciones de miles.
 Por ejemplo, si recibe 1234567890, debe devolver 1.234.567.890"""
+
+var = 1234567890
+print(f"{var:,}")
